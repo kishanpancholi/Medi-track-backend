c35b63ad7a37c558e67fe0deb25a12ea10489d82
kishanpancholi12 <kp.developer04@gmail.com>
2026-03-02 16:13:52 +0530

    done authentication only in patient login and registration.

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,22 +1,20 @@
 import Patient from "../models/Patient.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerPatient = async (req, res) => {
   try {
-    const {
-      fname,
-      lname,
-      gender,
-      dob,
-      mobile,
-      email,
-      address,
-      password,
-    } = req.body;
+    const { fname, lname, gender, dob, mobile, email, address, password } =
+      req.body;
 
     const exists = await Patient.findOne({ email });
     if (exists) {
       return res.status(400).json({ msg: "Email already registered" });
     }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10); // salt is use for aading the random string before the hashing It makes every password hash unique even two user use same password
+    const hashedPassword = await bcrypt.hash(password, salt); // it is conver the plain password into the secure hashing password
 
     const patient = await Patient.create({
       fname,
@@ -26,7 +24,7 @@ export const registerPatient = async (req, res) => {
       mobile,
       email,
       address,
-      password,
+      password: hashedPassword, // this is use for storing the hase password in DB
     });
 
     res.status(201).json({ msg: "Patient registered successfully!", patient });
@@ -35,7 +33,7 @@ export const registerPatient = async (req, res) => {
   }
 };
 
-//patient login 
+//patient login
 export const loginPatient = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -46,15 +44,26 @@ export const loginPatient = async (req, res) => {
       return res.status(400).json({ message: "Email not registered" });
     }
 
-    if (user.password !== password) {
+    const isMatch = await bcrypt.compare(password, user.password); // in here bcypt takes enter password hash that password again and match that password with stored hash password in DB if match then continue otherwise give
+    if (!isMatch) {
       return res.status(400).json({ message: "Incorrect Password" });
     }
+    // if (user.password !== password) {
+    //   return res.status(400).json({ message: "Incorrect Password" });
+    // }
+
+    // After login we create token:
+    const token = jwt.sign(
+      { id: user._id },
+      "secretkey123",//JWT secrat key and Digitally sign the token
+      { expiresIn: "1d" },
+    );
 
     res.status(200).json({
       message: "Login Successful",
+      token,
       user,
     });
-
   } catch (err) {
     res.status(500).json({ message: "Server Error" });
   }
--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { registerPatient, loginPatient } from "../controllers/patientController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerPatient);
 router.post("/login",loginPatient); 
+
+router.get("/profile", protect, async (req, res) => {
+   const user = await Patient.findById(req.user).select("-password");
+   res.json(user);
+});
 
 export default router;  

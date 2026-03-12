9bfe8882afbd0846f2d3d5dcbfbee75b0cf29fc9
kishanpancholi12 <kp.developer04@gmail.com>
2026-03-12 10:18:53 +0530

    small changes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const dbURI =
       "mongodb+srv://kishan:test123@cluster0.s4imkal.mongodb.net/healthcare?appName=Cluster0";
-    // const dbURI = "mongodb://127.0.0.1:27017/healthcare";
+      // "mongodb://127.0.0.1:27017/healthcare";
 
     await mongoose.connect(dbURI);
     console.log("DB Connected Successfully");

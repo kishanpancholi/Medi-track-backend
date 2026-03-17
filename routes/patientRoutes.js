fe1f9bf1b20ec058da0dda082b4b3fad475387dd
Sanjana <sanjanamali0903@gmail.com>
2026-03-17 11:16:59 +0530

    inside routes i have change patientRoutes.js file add import statement and create total patient count api

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,15 +1,28 @@
 import express from "express";
 import { registerPatient, loginPatient} from "../controllers/patientController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import Patient from "../models/Patient.js";
 
 const router = express.Router();
 
+//Register & Login
 router.post("/register", registerPatient);
 router.post("/login",loginPatient); 
 
+//Profile
 router.get("/profile", protect, async (req, res) => {
    const user = await Patient.findById(req.user).select("-password");
    res.json(user);
+});
+
+//Add Count
+router.get("/count", async (req, res) => {
+  try {
+    const totalPatients = await Patient.countDocuments();
+    res.json({ totalPatients });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;  

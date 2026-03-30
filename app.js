d031539c0e8c919dbe623c79f35c7bbe9abb63c2
kishanpancholi12 <kp.developer04@gmail.com>
2026-03-30 17:01:05 +0530

    change deploy code in app.js

--- app.js
+++ app.js
@@ -17,21 +17,6 @@ app.use(cors({
   credentials: true,
 })
 );
-// const allowedOrigins = [
-//   // "http://localhost:3000",
-//   process.env.FRONTEND_URL
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("CORS not allowed"));
-//     }
-//   },
-//   credentials: true
-// }));
 
 app.use(express.json());
 app.use(cookieParser());

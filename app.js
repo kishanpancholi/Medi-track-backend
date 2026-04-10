3a63f4fa47dc8f49d40b504f8d795d5254185f4b
kishanpancholi12 <kp.developer04@gmail.com>
2026-04-10 17:40:27 +0530

    create a server and setup room server code in app.js and install socket.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();// .env file ne load karse
+dotenv.config(); // .env file ne load karse
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import http from "http";
+import { Server } from "socket.io";
 
 //database connection
 connectDB();
@@ -12,10 +14,11 @@ connectDB();
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-})
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
 );
 
 app.use(express.json());
@@ -27,8 +30,48 @@ app.get("/", (req, res) => {
   res.send("Healthcare Backend Running...");
 });
 
+//create a http server
+const server = http.createServer(app);
+
+//SOCKET.IO SETUP
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  //room
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User join room: ${roomId}`);
+  });
+
+  // Offer
+  socket.on("offer", ({ offer, roomId }) => {
+    socket.to(roomId).emit("offer", offer);
+  });
+
+  // Answer
+  socket.on("answer", ({ answer, roomId }) => {
+    socket.to(roomId).emit("answer", answer);
+  });
+
+  // ICE Candidate
+  socket.on("ice-candidate", ({ candidate, roomId }) => {
+    socket.to(roomId).emit("ice-candidate", candidate);
+  });
+
+  // Disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

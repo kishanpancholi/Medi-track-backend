3fcafdcd47599bf8041642bd30d66f6520e265f2
Sanjana <sanjanamali0903@gmail.com>
2026-04-15 14:52:20 +0530

    remove code of count from patient controller and from routes file

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -3,9 +3,7 @@ import {
   registerPatient,
   loginPatient,
   completePatientProfile,
-  // getPatientCount,
   getPatients,
-  // getTodayPatients,
   deletePatient,
   logoutPatient,
 } from "../controllers/patientController.js";
@@ -22,22 +20,12 @@ router.put("/complete-profile", protect, authorize("patient"), completePatientPr
 
 router.post("/logout", logoutPatient);
 
-//Profile
-// router.get("/profile", protect, async (req, res) => {
-//    const user = await Patient.findById(req.user).select("-password");
-//    res.json(user);
-// });
-
 router.get("/profile", protect, authorize("patient"), (req, res) => {
   res.status(200).json({
     message: "Patient Authorized",
     user: req.user,
   });
 });
-
-// Add count
-// router.get("/count", getPatientCount);
-// router.get("/today", getTodayPatients);
 
 // Patient List APIs
 router.get("/list", getPatients); // GET all patients in admin side in patient page

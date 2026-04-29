a9e0dfdbf387f7f3a9433c683a79d7883c14672f
Sanjana <sanjanamali0903@gmail.com>
2026-04-29 10:47:59 +0530

    write code to update doctor profile create update api

--- controllers/medicalRecordController.js
+++ controllers/medicalRecordController.js
@@ -214,7 +214,7 @@ export const getDoctorPatients = async (req, res) => {
       doctor: doctorId
     }).select("patient");
 
-    console.log("Records Found:", records);
+    // console.log("Records Found:", records);
 
     const patientIds = [
       ...new Set(

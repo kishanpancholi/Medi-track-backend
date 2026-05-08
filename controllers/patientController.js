2ad24727ecfcff3bc73464655fe4ac039fdc1d45
Sanjana <sanjanamali0903@gmail.com>
2026-05-08 17:47:22 +0530

    change code to get patient details on admin side

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -154,13 +154,45 @@ const calculateAge = (dob) => {
 };
 
 // GET ALL PATIENTS (for Patient List page)
+// export const getPatients = async (req, res) => {
+//   try {
+//     const patients = await Patient.find()
+//        .select("-password")
+//       .sort({ createdAt: -1 });
+
+//     res.json(patients);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
 export const getPatients = async (req, res) => {
   try {
     const patients = await Patient.find()
-       .select("-password")
+      .select("-password")
       .sort({ createdAt: -1 });
 
-    res.json(patients);
+    // format data for frontend
+    const formattedPatients = patients.map((p) => ({
+      _id: p._id,
+
+      // Full Name
+      name: `${p.firstName} ${p.lastName}`,
+
+      // Calculate Age
+      age: calculateAge(p.dob),
+
+      // Other Fields
+      gender: p.gender,
+      phone: p.mobile,
+      email: p.email,
+      address: p.address,
+      city: p.city,
+      createdAt: p.createdAt,
+    }));
+
+    res.json(formattedPatients);
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
